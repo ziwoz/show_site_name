748bc2145146d7e56926d03eed7ec4f152cc9706
ziwoz <zameer.b.zubair@gmail.com>
2023-09-02 11:23:25 +0400

    figured out ways to insert scripts before and after the gunicorn

--- provision.sh
+++ provision.sh
@@ -10,7 +10,6 @@ sudo apt-get install -y python3 python3-pip python3-venv nginx
 sudo unlink /etc/nginx/sites-enabled/default
 
 
-# Create a Python virtual environment
 python3 -m venv /home/vagrant/myflaskappenv
 
 # Activate the virtual environment
@@ -38,13 +37,23 @@ After=network.target
 User=vagrant
 Group=vagrant
 WorkingDirectory=/home/vagrant/src/
-Environment="PATH=/home/vagrant/myflaskappenv/bin"
-ExecStart=/home/vagrant/myflaskappenv/bin/gunicorn --workers 3 --bind unix:/tmp/myflaskapp.sock -m 007 myflaskapp:app
-#ExecStart=/home/vagrant/myflaskappenv/bin/gunicorn --workers 3 --bind unix:/home/vagrant/myflaskapp.sock -m 007 myflaskapp:app
+# Environment="PATH=/home/vagrant/myflaskappenv/bin"
+# ExecStart=/home/vagrant/myflaskappenv/bin/gunicorn --workers 3 --bind unix:/var/www/myflaskapp.sock -m 007 myflaskapp:app
+# ExecStart=/home/vagrant/myflaskappenv/bin/gunicorn --workers 3 --bind unix:/var/www/sockets/myflaskapp.sock -m 007 myflaskapp:app
+# ExecStartPost='/bin/chmod 777 /var/www/myflaskapp.sock'
+Environment="PATH=/home/vagrant/myflaskappenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ExecStart=/home/vagrant/myflaskappenv/bin/gunicorn --workers 3 --bind unix:/var/www/myflaskapp.sock -m 007 myflaskapp:app
+ExecStartPost=/home/vagrant/postscript.sh
 
 [Install]
 WantedBy=multi-user.target
 EOL
+
+# Get the current user's primary group
+# Create the directory if it doesn't exist
+sudo mkdir -p /var/www/
+# Change the ownership of the directory to the current user and group
+sudo chown vagrant:vagrant /var/www/
 
 # Move the Gunicorn service file to the systemd service directory
 sudo mv /home/vagrant/gunicorn.service /etc/systemd/system/
@@ -53,9 +62,9 @@ sudo mv /home/vagrant/gunicorn.service /etc/systemd/system/
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 
-sleep 5
+sleep 5 # this 5 seconds is very very important
 
-sudo chmod 777 /tmp/myflaskapp.sock # move this file in a proper place and remove the 777
+sudo chmod 007 /var/www/myflaskapp.sock # move this inside a shell script and associate with the gunicorn.service
 
 # Set up Nginx to proxy pass to Gunicorn
 cat <<EOL | sudo tee /etc/nginx/sites-available/myflaskapp
@@ -65,8 +74,8 @@ server {
 
     location / {
         include proxy_params;
-        # proxy_pass http://unix:/home/vagrant/myflaskapp.sock;
-        proxy_pass http://unix:/tmp/myflaskapp.sock;
+        proxy_pass http://unix:/var/www/myflaskapp.sock;
+        # proxy_pass http://unix:/var/www/sockets/myflaskapp.sock;
     }
 }
 EOL
@@ -77,7 +86,7 @@ sudo ln -s /etc/nginx/sites-available/myflaskapp /etc/nginx/sites-enabled
 # Test Nginx config
 sudo nginx -t
 
-# Restart Nginx
+
 sudo systemctl restart nginx
 
 # Make the Nginx service start on boot
--- src/myflaskapp.py
+++ src/myflaskapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! machane"
+    return "Hello, World! freak machane"
 
 
 if __name__ == "__main__":
