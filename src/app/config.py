289595674228754f546ad3b3e26e6c7339a48297
zameer <zameer.b.zubair@gmail.com>
2023-09-14 07:59:40 +0400

    the database directory is dealt with with temporary fix. more will be done at the postgress time

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,11 @@
 import os
+import platform
 
 
 class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    if platform.system().lower() == 'linux':
+        SQLALCHEMY_DATABASE_URI = "sqlite:////var/www/site.db"
+        # move from this method to dynamic proper way. of having multiple config files and they load as per requirements based the vagrantfile
+
